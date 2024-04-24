3638a86c7544b4dbb614247d256a93125a8d9f69
chasoncai <chasoncai@tencent.com>
2024-04-25 01:19:44 +0800

    chore: add drf

--- home_application/urls.py
+++ home_application/urls.py
@@ -11,29 +11,44 @@ specific language governing permissions and limitations under the License.
 """
 
 from django.conf.urls import url
+from django.urls import path
+
 
 from . import views
 
-# example
+# urlpatterns = (
+#     url(r"^$", views.home),
+#     url(r"^search/$", views.home),
+#     url(r"^backup/$", views.home),
+#     url(r"^test-json/$", views.test_json),
+#     url(r"^sync-cmdb/$", views.sync_cmdb),
+#     url(r"^business/$", views.get_business),
+#     url(r"^set$", views.get_sets_by_biz),
+#     url(r"^module$", views.get_modules_by_set),
+#     url(r"^hosts$", views.get_hosts),
+#     url(r"^host-info$", views.get_host_info),
+#     url(r"^search-files$", views.search_files),
+#     url(r"^backup-files$", views.backup_files),
+#     url(r"^backup-records$", views.backup_records),
+#     url(r"^topo$", views.topo_tree),
+#     url(r"^iam/api/v1/business/$", views.iam_business),
+# )
+
 urlpatterns = (
-    url(r"^$", views.home),
-    url(r"^search/$", views.home),
-    url(r"^backup/$", views.home),
-    url(r"^test-json/$", views.test_json),
-    url(r"^sync-cmdb/$", views.sync_cmdb),
-    url(r"^business/$", views.get_business),
-    url(r"^set$", views.get_sets_by_biz),
-    url(r"^module$", views.get_modules_by_set),
-    url(r"^hosts$", views.get_hosts),
-    url(r"^host-info$", views.get_host_info),
-    url(r"^search-files$", views.search_files),
-    url(r"^backup-files$", views.backup_files),
-    url(r"^backup-records$", views.backup_records),
-    url(r"^topo$", views.topo_tree),
-    url(r"^iam/api/v1/business/$", views.iam_business),
+    path("/", views.home),
+    path("search/", views.home),
+    path("backup/", views.home),
+    path("test-json/", views.test_json),
+    path("sync-cmdb/", views.sync_cmdb),
+    path("business/", views.get_business),
+    path("set/", views.get_sets_by_biz),
+    path("module/", views.get_modules_by_set),
+    path("hosts/", views.get_hosts),
+    path("host-info/", views.get_host_info),
+    path("search-files/", views.search_files),
+    path("backup-files/", views.backup_files),
+    path("backup-records/", views.backup_records),
+    path("topo/", views.topo_tree),
+    path("iam/api/v1/business/", views.iam_business),
 )
 
-# urlpatterns = [
-#     url(r'^$', views.hello),
-#     url(r'^test-json/$', views.test_json)
-# ]
